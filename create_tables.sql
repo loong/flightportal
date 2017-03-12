f701eafb99f3d64724f9242580dfab569adc7b7a
Long Hoang <long@mindworker.de>
2017-03-12 17:38:56 +0800

    Add flightdiary comments to database

--- create_tables.sql
+++ create_tables.sql
@@ -15,6 +15,18 @@ CREATE TABLE IF NOT EXISTS airports(
        source varchar
 );
 
+CREATE TABLE IF NOT EXISTS airport_rankings(
+       IATA char(3) NOT NULL UNIQUE,
+       rank int,
+       passengers int
+);
+
+CREATE TABLE IF NOT EXISTS airport_spatial(
+       id int PRIMARY KEY UNIQUE,
+       location geography(Point,4326)
+);
+
+
 CREATE TABLE IF NOT EXISTS flightdiary_airports(
        name varchar,
        url varchar,
@@ -28,13 +40,22 @@ CREATE TABLE IF NOT EXISTS flightdiary_airports(
        id int PRIMARY KEY UNIQUE
 );
 
-CREATE TABLE IF NOT EXISTS airport_rankings(
-       IATA char(3) NOT NULL UNIQUE,
-       rank int,
-       passengers int
-);
-
-CREATE TABLE IF NOT EXISTS airport_spatial(
+CREATE TABLE IF NOT EXISTS flightdiary_comments(
+       _type varchar,
+       airport_id int,
+       content text,
+       extra varchar,
+       facebook varchar,
+       helpful_percentage int,
        id int PRIMARY KEY UNIQUE,
-       location geography(Point,4326)
+       is_voted boolean,
+       name varchar,
+       page int,
+       rating smallint,
+       reported varchar,
+       time varchar,
+       timestamp varchar,
+       total_no int,
+       total_yes int,
+       url varchar
 );

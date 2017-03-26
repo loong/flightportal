a7c751af441ac87fe01ea11cc6af9f749f90e2aa
Long Hoang <long@mindworker.de>
2017-03-27 00:07:42 +0800

    Improve FP-Growth input and parameters

--- insights/fp-growth-flights.py
+++ insights/fp-growth-flights.py
@@ -1,4 +1,7 @@
+import sys
+
 from fp_growth import find_frequent_itemsets
+from pprint import pprint
 
 """Notes:
 
@@ -11,18 +14,25 @@ from fp_growth import find_frequent_itemsets
 
 """
 
+if len(sys.argv) < 2:
+    print("Please provide airport IATA code. e.g.")
+    print("\n\tpython fp-growth-flights.py HKG\n")
+    exit(-1)
+
+airport = sys.argv[1]
+
 transactions = []
 with open("../datastore/results/flight_transactions") as f:
     for line in f:
         trans = line.split(",")
         trans = list(set(trans))                             # Remove duplicates
+        # trans = filter(lambda x: airport in x, trans)
         trans = map(lambda x: set(x.split("->")), trans)     # Do not consider direction
         trans = map(lambda x: '-'.join(x), trans)
-
-        if "SYD" in ''.join(trans):                          # Filter out one airport
+        if airport in ' '.join(trans):                       # Filter out one airport
             transactions.append(trans)
 
-print("Process %s of transactions" % len(transactions))
+print("Process %s of transactions for %s" % (len(transactions), airport))
 
 # Obscure data science voodoo
 min_sup = 30+len(transactions)/10
@@ -32,14 +42,13 @@ if min_sup > 130:
 # fire FP Growth mining
 res = find_frequent_itemsets(transactions, min_sup, True)
 
-i = 0
-for r in res:
-    freq_set = r[0]
+# filter and sort results
+result = filter(lambda x: len(x[0]) > 2, res)                # only keep sets with at least 3 items
+result = filter(lambda x: airport in ' '.join(x[0]), result) # final set must include airport
+result.sort(key=lambda x: x[1], reverse=True)
 
-    if len(freq_set) > 2:
-        i += 1
-        print(r)
+pprint(result)
 
 print("Done\n")
-print("%s frequent sets found with minimal support of %s in %s transactions" % (i, round(min_sup, 2), len(transactions)))
+print("%s frequent sets found with minimal support of %s in %s transactions" % (len(result), round(min_sup, 2), len(transactions)))
 
